25a3f2710ececafc59258c9ce2d8915805d920ab
marioharison51 <marioharison51@gmail.com>
2026-08-17 15:30:13 +0300

    Create Course.php

---
+++ app/Models/Course.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'subject',
+        'class_name',
+        'description',
+        'teacher_id',
+    ];
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+}
