246cd87297dfbad924e101f248325b9d15e92910
marioharison51 <marioharison51@gmail.com>
2026-08-17 19:16:07 +0300

    Create Grade.php

---
+++ app/Models/Grade.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Grade extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'exam_id',
+        'student_id',
+        'score',
+        'comment',
+    ];
+
+    protected $casts = [
+        'score' => 'decimal:2',
+    ];
+
+    public function exam(): BelongsTo
+    {
+        return $this->belongsTo(Exam::class);
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
