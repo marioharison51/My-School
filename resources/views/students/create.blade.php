f61479cd130858fce82664ef907cb31573160810
marioharison51 <marioharison51@gmail.com>
2026-08-17 15:03:45 +0300

    Update create.blade.php

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,20 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Ajouter un élève
+            Inscription d'un élève
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <form action="{{ route('students.store') }}" method="POST" class="space-y-4">
-                    @csrf
+                <form method="POST" action="{{ route('students.store') }}">
                     @include('students._form')
-
-                    <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
-                        Enregistrer
-                    </button>
                 </form>
             </div>
         </div>
