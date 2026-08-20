3e7d5704a161f8a7f5c81a2a9db68a1fa7bbccf9
marioharison51 <marioharison51@gmail.com>
2026-08-20 08:07:57 +0300

    Update create.blade.php

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -51,7 +51,7 @@
                     </div>
 
                     <div class="flex justify-end">
-                        <button type="submit" class="px-4 py-2 bg-gray-900 text-white rounded">
+                        <button type="submit" class="px-4 py-2 bg-primary-700 hover:bg-primary-600 text-white rounded">
                             Enregistrer le paiement
                         </button>
                     </div>
