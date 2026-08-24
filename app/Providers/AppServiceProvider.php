204b0ddc2923eed7a31b75dc19c1361c47fc1712
marioharison51 <marioharison51@gmail.com>
2026-08-24 13:43:07 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            \App\Services\Sms\SmsGateway::class,
+            \App\Services\Sms\LogSmsGateway::class
+        );
     }
 
     /**
