ebbc2779bb16c3c28a066edbbaacaba03c182393
Hari-Mael <ismael.harison@outlook.fr>
2026-08-25 01:02:26 +0300

    Adjust layout width for student dashboard view

--- resources/views/dashboards/eleve.blade.php
+++ resources/views/dashboards/eleve.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             @if (! $student)
                 <div class="bg-yellow-100 text-yellow-800 rounded-lg p-6">
