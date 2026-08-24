f73b21b5b369d8c57b42895f558912b0438a2015
marioharison51 <marioharison51@gmail.com>
2026-08-24 23:07:42 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,15 +7,26 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'email',
+    'password',
+    'role',
+    'account_status',
+    'suspended_until',
+    'blocked_reason',
+    'exam_dates_hidden',
+    'requires_password_reset',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * Get the attributes that should be cast.
@@ -27,6 +38,9 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'suspended_until' => 'date',
+            'exam_dates_hidden' => 'boolean',
+            'requires_password_reset' => 'boolean',
         ];
     }
 
