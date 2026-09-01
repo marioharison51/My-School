f5dde692dddc132014fdd8e2fbebea7c75d45294
Hari Mael <ismael.harison@outlook.fr>
2026-09-01 14:59:08 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/bulletins/show.blade.php
+++ resources/views/bulletins/show.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Bulletin — {{ $student->full_name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Bulletin — {{ $student->full_name }}">
+<div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             <div class="bg-white shadow-sm rounded-lg p-6">
@@ -71,4 +65,4 @@
 
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
