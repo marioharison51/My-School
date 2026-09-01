377e2f870bf29c4ddbc41dac61d9de6e9c98bfe5
Hari Mael <ismael.harison@outlook.fr>
2026-09-01 15:08:02 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/dashboards/comptable.blade.php
+++ resources/views/dashboards/comptable.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Tableau de bord — Comptable
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Tableau de bord — Comptable">
+<div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -71,4 +65,4 @@
 
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
