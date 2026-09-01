3604c077ecb97be0869bf507aa78c375bf74ff23
Hari Mael <ismael.harison@outlook.fr>
2026-09-01 14:56:41 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Annonces & Événements
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Annonces & Événements">
+<div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
 
             @if (session('success'))
@@ -55,4 +49,4 @@
 
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
