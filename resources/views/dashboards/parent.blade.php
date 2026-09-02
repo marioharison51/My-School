2b82ef0ad44569a8ab8447fc4b2c0e30a9eed43a
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:07:14 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/dashboards/parent.blade.php
+++ resources/views/dashboards/parent.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Tableau de bord — Parent
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Tableau de bord — Parent">
+<div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
             @forelse ($children as $child)
@@ -52,4 +46,4 @@
 
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
