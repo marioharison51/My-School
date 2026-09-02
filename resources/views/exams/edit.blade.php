43353e984c6878007983b0b6eb2d88e9a0b4bb36
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:09:29 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/exams/edit.blade.php
+++ resources/views/exams/edit.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Modifier l'examen — {{ $exam->title }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Modifier l'examen — {{ $exam->title }}">
+<div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <form method="POST" action="{{ route('exams.update', $exam) }}">
@@ -15,4 +9,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
