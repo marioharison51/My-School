a148dfa0bd054a4231b1939fd7e5f948b6636107
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:10:56 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/exams/index.blade.php
+++ resources/views/exams/index.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Examens
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Examens">
+<div class="py-12">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
             @if (session('status'))
@@ -68,4 +62,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
