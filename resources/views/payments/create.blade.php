4a8f1cd7974bad1d3a9119b324639bb39c385779
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:18:46 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Nouveau paiement — {{ $student->full_name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Nouveau paiement — {{ $student->full_name }}">
+<div class="py-12">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
 
@@ -70,4 +64,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
