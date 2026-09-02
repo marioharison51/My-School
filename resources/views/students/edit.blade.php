c02b331df97633fc39e5a9a05bfdc292f9ea271b
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:26:36 +0300

    Replace x-app-layout with x-sidebar-layout in edit view

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Modifier la fiche de {{ $student->first_name }} {{ $student->last_name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Modifier la fiche de {{ $student->first_name }} {{ $student->last_name }}">
+<div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <form method="POST" action="{{ route('students.update', $student) }}">
@@ -15,4 +9,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
