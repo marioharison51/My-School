824b034f1a5edb566214e2456883c50a9249f713
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:16:36 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $message->subject }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="{{ $message->subject }}">
+<div class="py-12">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
 
@@ -32,4 +26,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
