c99eb6cf60b2cf5cd85f78d5fac21179bb44ad9f
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:08:25 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/exams/create.blade.php
+++ resources/views/exams/create.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Planifier un examen
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Planifier un examen">
+<div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <form method="POST" action="{{ route('exams.store') }}">
@@ -14,4 +8,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
