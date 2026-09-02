1c8a34d7a073106758c023b194dd8050032ce3bb
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:25:41 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Inscription d'un élève
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Inscription d'un élève">
+<div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <form method="POST" action="{{ route('students.store') }}">
@@ -14,4 +8,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
