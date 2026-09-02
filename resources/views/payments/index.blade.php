22f8f8d31efbe7db8793c4e431551dbd857c1883
Hari Mael <ismael.harison@outlook.fr>
2026-09-02 07:20:00 +0300

    Replace x-app-layout with x-sidebar-layout

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,11 +1,5 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Paiements — {{ $student->full_name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+<x-sidebar-layout title="Paiements — {{ $student->full_name }}">
+<div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
 
             @if (session('success'))
@@ -60,4 +54,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-sidebar-layout>
